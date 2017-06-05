21dde36c78fdeb8b12d614532021690b8131d92e
Austen Madden <amadden@covermymeds.com>
2017-06-04 21:31:54 -0400

    Fun ruby class that implements some problems and algorithms

---
+++ fun.rb
@@ -0,0 +1,89 @@
+require 'pry'
+
+class Fun
+  class << self
+    def is_palindrome?(word)
+      word.chars.each_index do |i|
+        return false if word[i] != word[word.length - i]
+      end
+    end
+
+    def fizzbuzz(range)
+      for i in 0..range
+        string = ""
+        string << "fizz" if i % 3 == 0
+        string << "buzz" if i % 5 == 0
+
+        unless string.empty?
+          puts string
+        else
+          puts i
+        end
+      end
+    end
+
+    def roman_numeral_writer(input)
+      numbers_with_numerals = {
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1
+      }
+
+      numeral = ''
+
+      numbers_with_numerals.each do |k, v|
+        div, rem = input.divmod(v)
+        if div > 0
+          div.times { numeral << k }
+          input = rem
+        end
+      end
+
+      numeral
+    end
+
+    def max(x, y)
+      (x > y) ? x : y
+    end
+
+    def kandane(array = [])
+      max_ending_here = max_so_far = array[0]
+
+      for x in array[1..-1]
+        max_ending_here = max(x, max_ending_here + x)
+        max_so_far = max(max_so_far, max_ending_here)
+      end
+
+      max_so_far
+    end
+
+    def kandane_array(array = [])
+      max_ending_here = max_so_far = array[0]
+      i1, i2 = 0
+
+      array[1..-1].each_with_index do |x, i|
+        max_ending_here = max(x, max_ending_here + x)
+        if max_so_far <= max(max_so_far, max_ending_here)
+          max_so_far = max_ending_here
+          if max_ending_here == x
+            i1 = i2 = i + 1
+          else
+            i2 = i + 1
+          end
+        end
+      end
+
+      array[i1..i2]
+    end
+  end
+end
